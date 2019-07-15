072c9495b77f7e7bc2e668b5e0ded48209879d75
Ariel Zusman <azusman@obil-azusman-2.local>
2019-07-15 20:45:05 +0300

    add test for authController signup

--- back/api/auth/__tests__/auth.controller.spec.js
+++ back/api/auth/__tests__/auth.controller.spec.js
@@ -15,6 +15,9 @@ const mockResponse = () => {
     return res;
 };
 describe('Auth controller', () => {
+    beforeAll(() => {
+        logger.debug = jest.fn();
+    });
     describe('login() function', () => {
         it('should return authenticated user', async () => {
             expect.assertions(2);
@@ -49,7 +52,6 @@ describe('Auth controller', () => {
             const req = mockRequest({ email: 'invalidUser@test.com' });
             const res = mockResponse();
 
-
             // make the authService.login reject
             jest.spyOn(authService, 'login').mockRejectedValue('ERROR MSG');
 
@@ -57,6 +59,55 @@ describe('Auth controller', () => {
 
             expect(res.status).toHaveBeenCalledWith(401);
             expect(res.send).toHaveBeenCalledWith({ error: 'ERROR MSG' });
+        });
+    });
+
+    describe('singup() function', () => {
+        it('should return registered user', async () => {
+            expect.assertions(3);
+            const req = mockRequest({
+                email: 'test.user@test.com',
+                username: 'testUser',
+                password: '123456'
+            });
+
+            const res = mockResponse();
+
+            const authServiceResponse = {
+                _id: '5d2992c4d6d64b42a48422d9',
+                email: 'test.user@test.com',
+                username: 'testUser'
+            };
+
+            jest.spyOn(authService, 'signup').mockResolvedValue(
+                authServiceResponse
+            );
+
+            await signup(req, res);
+
+            expect(req.session.user).toEqual(authServiceResponse);
+            expect(res.status).toHaveBeenCalledWith(200);
+            expect(res.send).toHaveBeenCalledWith(authServiceResponse);
+        });
+
+        it('should return status 500 when user details invalid', async () => {
+            expect.assertions(2);
+
+            const req = mockRequest({
+                email: 'test.user@test.com',
+                password: '123456'
+            });
+
+            const res = mockResponse();
+
+            jest.spyOn(authService, 'signup').mockRejectedValue('ERROR MSG');
+
+            await signup(req, res);
+
+            expect(res.status).toHaveBeenCalledWith(500);
+            expect(res.send).toHaveBeenCalledWith({
+                error: 'could not signup, please try later'
+            });
         });
     });
 });
